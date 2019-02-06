d490869ea63eaf0f017fc73954458568aa9743e0
satyapaul <satyajit.paul@gmail.com>
2019-02-06 16:57:25 +0530

    Delete ResponseProcessor.java

--- src/com/restapi/client/ResponseProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.restapi.client;
-
-
-import javax.ws.rs.core.MultivaluedMap;
-
-public interface ResponseProcessor {
-	public String getResponseBody();
-	public MultivaluedMap<String, Object> getResponseHeaders();
-}
